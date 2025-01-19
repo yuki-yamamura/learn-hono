f119230d9563db0070340e31f6a0f0d2663a4365
Yuki Yamamura <126521201+yuki-yamamura@users.noreply.github.com>
2025-01-19 13:51:49 +0900

    configure swagger

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import { cors } from "hono/cors";
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { companiesRoute } from "@/api/companies";
+import { swaggerUI } from "@hono/swagger-ui";
 
 const app = new OpenAPIHono();
 
@@ -12,5 +13,14 @@ app.use(
 );
 
 app.route("/companies", companiesRoute);
+
+app.get("/ui", swaggerUI({ url: "/doc" }));
+app.doc("/doc", {
+  info: {
+    title: "An API",
+    version: "v1",
+  },
+  openapi: "3.1.0",
+});
 
 export default app;
